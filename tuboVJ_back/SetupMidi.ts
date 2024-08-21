27c4f92d7f8ef57244f8715a8319f18e0e473c17
Sebastien Maire <sebastien.maire@epitech.eu>
2024-08-21 13:45:53 +0200

    Cleaning midi implem and made it autorestart

---
+++ tuboVJ_back/SetupMidi.ts
@@ -0,0 +1,55 @@
+import midi from 'midi'
+
+export interface KorgNanoKontrol2_VerticalAreaState {
+    fader: number;
+    knob: number;
+    sButton: number;
+    mButton: number;
+    rButton: number;
+}
+export interface KorgNanoKontrol2_State {
+    rightSide: KorgNanoKontrol2_VerticalAreaState[];
+}
+
+let midiInput: midi.Input | undefined = undefined;
+
+export function ResetMidi(midiState: KorgNanoKontrol2_State) {
+    if (midiInput) {
+        midiInput.closePort();
+    }
+    midiInput = new midi.Input();
+    midiInput.on('message', (deltaTime, message) => {
+        // The message is an array of numbers corresponding to the MIDI bytes:
+        //   [status, data1, data2]
+        // https://www.cs.cf.ac.uk/Dave/Multimedia/node158.html has some helpful
+        // information interpreting the messages.
+        if (message[1] >= 16 && message[1] <= 23) {
+            // knob
+            const targetIndex = message[1] - 16;
+            midiState.rightSide[targetIndex].knob = message[2] / 127.0;
+        }
+        else if (message[1] >= 0 && message[1] <= 7) {
+            // Fader
+            const targetIndex = message[1];
+            midiState.rightSide[targetIndex].fader = message[2] / 127.0;
+        }
+        else if (message[1] >= 32 && message[1] <= 39) {
+            // S button
+            const targetIndex = message[1] - 32;
+            midiState.rightSide[targetIndex].sButton = message[2] / 127.0;
+        }
+        else if (message[1] >= 48 && message[1] <= 55) {
+            // M button
+            const targetIndex = message[1] - 48;
+            midiState.rightSide[targetIndex].mButton = message[2] / 127.0;
+        }
+        else if (message[1] >= 64 && message[1] <= 71) {
+            // R button
+            const targetIndex = message[1] - 64;
+            midiState.rightSide[targetIndex].rButton = message[2] / 127.0;
+        }
+        console.log(midiState);
+        // console.log(`m: ${message} d: ${deltaTime}`);
+    });
+    midiInput.openPort(0);
+}
--- tuboVJ_back/index.ts
+++ tuboVJ_back/index.ts
@@ -6,17 +6,8 @@ import { SetupHardwareMonitoring, HARDWARE_STATS, HardwareStats } from './SetupH
 import websocket from '@fastify/websocket'
 import * as WebSocket from 'ws';
 import midi from 'midi'
+import { KorgNanoKontrol2_State, ResetMidi } from './SetupMidi'
 
-interface KorgNanoKontrol2_VerticalAreaState {
-    fader: number;
-    knob: number;
-    sButton: number;
-    mButton: number;
-    rButton: number;
-}
-interface KorgNanoKontrol2_State {    
-    rightSide: KorgNanoKontrol2_VerticalAreaState[];
-}
 
 const midiState: KorgNanoKontrol2_State = {
     rightSide: []
@@ -32,44 +23,9 @@ for (let i = 0 ; i < 8; ++i) {
     })
 }
 
-const midiInput = new midi.Input();
-midiInput.on('message', (deltaTime, message) => {
-    // The message is an array of numbers corresponding to the MIDI bytes:
-    //   [status, data1, data2]
-    // https://www.cs.cf.ac.uk/Dave/Multimedia/node158.html has some helpful
-    // information interpreting the messages.
-    if (message[1] >= 16 && message[1] <= 23) {
-        // knob
-        const targetIndex = message[1] - 16;
-        midiState.rightSide[targetIndex].knob = message[2] / 127.0;
-    }
-    else if (message[1] >= 0 && message[1] <= 7) {
-        // Fader
-        const targetIndex = message[1];
-        midiState.rightSide[targetIndex].fader = message[2] / 127.0;
-    }
-    else if (message[1] >= 32 && message[1] <= 39) {
-        // S button
-        const targetIndex = message[1] - 32;
-        midiState.rightSide[targetIndex].sButton = message[2] / 127.0;
-    }
-    else if (message[1] >= 48 && message[1] <= 55) {
-        // M button
-        const targetIndex = message[1] - 48;
-        midiState.rightSide[targetIndex].mButton = message[2] / 127.0;
-    }
-    else if (message[1] >= 64 && message[1] <= 71) {
-        // R button
-        const targetIndex = message[1] - 64;
-        midiState.rightSide[targetIndex].rButton = message[2] / 127.0;
-    }
-    console.log(midiState);
-    // console.log(`m: ${message} d: ${deltaTime}`);
-  });
-  
-  // Open the first available input port.
-  midiInput.openPort(0);
-
+setInterval(()=>{
+    ResetMidi(midiState);
+}, 1000)
 
 const connectedSockets: WebSocket.WebSocket[] = [];
 
